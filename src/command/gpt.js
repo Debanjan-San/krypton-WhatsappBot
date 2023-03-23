ac77bd90e808e7e568cd167f046a8db44df331e1
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-03-23 23:40:44 +0530

    Update gpt.js

--- src/command/gpt.js
+++ src/command/gpt.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'gpt',
     aliases: ['g'],
+    category: 'unity',
     description: 'Let you chat with GPT chat bot',
     async execute(client, arg, M) {
         const res = await client.AI.gpt(arg)
