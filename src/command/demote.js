579a427d9df88787cf8f2001758fb75df7b50a03
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-03-23 23:38:47 +0530

    Update demote.js

--- src/command/demote.js
+++ src/command/demote.js
@@ -4,6 +4,7 @@ module.exports = {
     admin: true,
     public: false,
     botAdmin: true,
+    category: 'moderation' 
     description: 'Promotes the taged user',
     async execute(client, arg, M) {
         if (M.mentions == 0) return M.reply('You must tag the user before using!')
