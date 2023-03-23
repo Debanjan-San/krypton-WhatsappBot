948f9cd70f9b3878f8b23209c67b9224c189644d
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-03-23 23:37:26 +0530

    Update alive.js

--- src/command/alive.js
+++ src/command/alive.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'alive',
     aliases: ['a'],
+    category: 'general',
     description: 'Testing stuff',
     async execute(client, arg, M) {
         M.reply(`Everything is working ${(await client.contact.getContact(M.sender, client)).username}`);
