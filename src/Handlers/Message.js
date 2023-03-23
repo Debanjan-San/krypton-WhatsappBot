4b73b264286405cddb634351a74bb903c37f110d
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-03-23 21:48:18 +0530

    Used formatter

--- src/Handlers/Message.js
+++ src/Handlers/Message.js
@@ -1,9 +1,21 @@
-const { getBinaryNodeChild } = require('@adiwajshing/baileys')
-const { serialize } = require('../lib/WAclient')
-const { color } = require('../lib/colour')
-const { Collection } = require('discord.js')
-const { readdirSync } = require('fs-extra')
-const { join } = require('path')
+const {
+    getBinaryNodeChild
+} = require('@adiwajshing/baileys')
+const {
+    serialize
+} = require('../lib/WAclient')
+const {
+    color
+} = require('../lib/colour')
+const {
+    Collection
+} = require('discord.js')
+const {
+    readdirSync
+} = require('fs-extra')
+const {
+    join
+} = require('path')
 
 module.exports = MessageHandler = async (messages, client) => {
     try {
@@ -12,11 +24,16 @@ module.exports = MessageHandler = async (messages, client) => {
         if (!M.message) return;
         if (M.key && M.key.remoteJid === "status@broadcast") return;
         if (M.type === "protocolMessage" || M.type === "senderKeyDistributionMessage" || !M.type || M.type === "") return;
-        
+
         client.prefix = '!'
         client.cmd = new Collection()
 
-        const { isGroup, sender, from, body } = M;
+        const {
+            isGroup,
+            sender,
+            from,
+            body
+        } = M;
         const gcMeta = isGroup ? await client.groupMetadata(from) : "";
         const gcName = isGroup ? gcMeta.subject : "";
         const args = body.trim().split(/ +/).slice(1);
@@ -38,17 +55,17 @@ module.exports = MessageHandler = async (messages, client) => {
          */
         const commandFiles = readdirSync(join(__dirname, '..', 'command')).filter((file) => file.endsWith('.js'))
         for (const file of commandFiles) {
-               /**
-                 * @constant
-                 * @type {commandFiles}
-                 */
+            /**
+             * @constant
+             * @type {commandFiles}
+             */
             const command = require(join(__dirname, '..', 'command', file))
             client.cmd.set(command.name, command)
         }
         if (!isCmd) return
         const command =
-        client.cmd.get(cmdName) ||
-        client.cmd.find((cmd) => cmd.aliases && cmd.aliases.includes(cmdName))
+            client.cmd.get(cmdName) ||
+            client.cmd.find((cmd) => cmd.aliases && cmd.aliases.includes(cmdName))
 
         if (!command) return M.reply("No such command found! BAKA");
         if (!groupAdmins.includes(sender) && command?.admin) return M.reply("This command can only be used by group or community admins")
@@ -58,7 +75,7 @@ module.exports = MessageHandler = async (messages, client) => {
         if (!command?.public) return M.reply("This command can only be used in public chat")
         command.execute(client, arg, M)
         //console.log(command)
-      } catch(err) {
-      console.log(err)
-   }
+    } catch (err) {
+        console.log(err)
+    }
 }
