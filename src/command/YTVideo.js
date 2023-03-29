22791b910a57ccbda21279b989c7d4c090c3f9fc
Dasabae <debanjan07@outlook.com>
2023-03-29 14:53:32 +0530

    few updates in media commands now it can be accessed without link

--- src/command/YTVideo.js
+++ src/command/YTVideo.js
@@ -1,4 +1,5 @@
 const YT = require('../lib/YT')
+const yts = require('yt-search')
 
 module.exports = {
     name: 'ytvideo',
@@ -7,9 +8,17 @@ module.exports = {
     exp: 5,
     description: 'Downloads given YT Video',
     async execute(client, arg, M) {
-        if (!arg) return M.reply('Please use command with a valid youtube.com link')
-        if (!YT.validateURL(arg.trim())) return M.reply('Please use this command with a valid youtube.com link')
-        const { videoDetails } = await YT.getInfo(arg)
+        const link = async (term) => {
+            const { videos } = await yts(term.trim())
+            if (!videos || !videos.length) return null
+            return videos[0].url
+        }
+        if (!arg) return M.reply('Please use this command with a valid youtube.com link')
+        const validPathDomains = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/
+        const term = validPathDomains.test(arg) ? arg.trim() : await link(arg)
+        if (!term) return M.reply('Please use this command with a valid youtube contant link')
+        if (!YT.validateURL(term.trim())) return M.reply('Please use this command with a valid youtube.com link')
+        const { videoDetails } = await YT.getInfo(term)
         M.reply('Downloading has started please have some pesence')
         let text = `*Title:* ${videoDetails.title} | *Type:* Video | *From:* ${videoDetails.ownerChannelName}`
         client.sendMessage(
@@ -25,7 +34,7 @@ module.exports = {
             }
         )
         if (Number(videoDetails.lengthSeconds) > 1800) return M.reply('Cannot download video longer than 30 minutes')
-        const audio = YT.getBuffer(arg, 'video')
+        const audio = YT.getBuffer(term, 'video')
             .then(async (res) => {
                 await client.sendMessage(
                     M.from,
