81148731b5e3b803981069c62d7f1aba641bd490
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2023-04-13 15:02:02 +0530

    Update update.js

--- src/Commands/dev/update.js
+++ src/Commands/dev/update.js
@@ -1,16 +1,39 @@
+const simpleGit = require('simple-git')
+const git = simpleGit()
+
 module.exports = {
-    name: 'update',
-    aliases: ['up'],
+    name: 'updates',
+    aliases: ['updatenow'],
     category: 'dev',
     exp: 0,
-    description: 'Updates the bot',
+    description: 'Updates gives the list of latest commits and updatenow updates the bot',
     async execute(client, arg, M) {
-        M.reply('Updating...')
-        await client.utils
-            .term(
-                'git remote set-url origin https://github.com/Debanjan-San/krypton-WhatsappBot.git && git pull origin main && npm install'
-            )
-            .then((res) => M.reply(res))
-            .catch((err) => M.reply(err.message))
+        const command = M.body.split(' ')[0].toLowerCase().slice(client.prefix.length).trim()
+        await git.fetch()
+        const commits = await git.log(['main' + '..origin/' + 'main'])
+        if (command == 'updates') {
+            let updates = '======= *UPDATES* =======\n\n'
+            if (commits.total == 0) return M.reply('Sorry there is no new updates!!')
+            commits['all'].map((commit) => {
+                updates +=
+                    '```🔹 [' +
+                    commit.date.substring(0, 10) +
+                    ']: ' +
+                    commit.message +
+                    ' <' +
+                    commit.author_name +
+                    '>```\n'
+            })
+            M.reply(updates)
+        }
+        if (command == 'updatenow') {
+            if (commits.total == 0) return M.reply('You are already using the updated version')
+            git.pull(async (err, update) => {
+                if (update && update.summary.changes) {
+                    await M.reply('```Updateing....```')
+                    await client.utils.term('npm install').stderr.pipe(process.stderr)
+                } else if (err) return M.reply(err)
+            })
+        }
     }
 }
