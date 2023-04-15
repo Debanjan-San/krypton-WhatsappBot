aa10b03de14634fb1aa98635b44c1df0e6174577
Dasabae <debanjan07@outlook.com>
2023-04-15 13:42:51 +0530

    fix ?

--- src/Commands/utils/removebg.js
+++ src/Commands/utils/removebg.js
@@ -7,11 +7,12 @@ module.exports = {
     async execute(client, arg, M) {
         if (!client.bgAPI) return M.reply("You didn't provide an api key")
         const content = JSON.stringify(M.quoted)
-        const isImage = M.quoted
+        const isQuoted = M.type === 'extendedTextMessage' && content.includes('imageMessage')
+        const isImage = isQuoted
             ? M.type === 'extendedTextMessage' && content.includes('imageMessage')
             : M.type === 'imageMessage'
         if (!isImage) return M.reply("You didn't provide an image")
-        const buffer = isImage ? await M.quoted.download() : await M.download()
+        const buffer = isQuoted ? await M.quoted.download() : await M.download()
         const image = await client.utils.removeBG(buffer)
 
         await client.sendMessage(
