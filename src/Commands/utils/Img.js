8c131737a48d3eac0d3ee5a5cae7509923efa2b8
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-04-17 11:13:03 +0500

    chore: add toimg
    didn't tested

---
+++ src/Commands/utils/Img.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: 'toimg',
+    aliases: ['img'],
+    category: 'utils',
+    exp: 10,
+    description: 'Converts sticker to image/gif',
+    async execute(client, arg, M) {
+        if (!M.quoted || (M.quoted && M.quoted.type !== 'stickerMessage'))
+            return void M.reply('*Quote the sticker that you want to convert, Baka!*')
+        const buffer = await M.quoted.download()
+        const animated = M.quoted?.message?.stickerMessage?.isAnimated
+        const type = animated ? 'video' : 'image'
+        try {
+            const result = animated
+                ? await client.utils.webpToMp4(buffer)
+                : await client.utils.webpToPng(buffer)
+            await client.sendMessage(M.from, {
+                [type]: result,
+                caption: 'Converted successful',
+                gifPlayback: animated ? true : undefined
+            })
+        } catch (error) {
+            client.log(error, 'red')
+            return await M.reply('*Try Again*')
+        }
+    }
+}
