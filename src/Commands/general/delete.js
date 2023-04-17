0b8b085224ddbf7e8414e9d1626f54187140fbc5
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-04-17 16:35:51 +0500

    chore: update
    for now

---
+++ src/Commands/general/delete.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'delete',
+    aliases: ['del'],
+    category: 'general',
+    exp: 5,
+    description: 'Deletes the quoted message',
+    async execute(client, arg, M) {
+        if (!M.quoted) return M.reply('Quote the message that you want me to delete, Baka!')
+        await client.sendMessage(M.from, {
+            delete: M.quoted.key
+        })
+    }
+}
