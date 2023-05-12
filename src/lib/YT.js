d767504fc5be590159d5f2dbb89147198fcc2113
Lakshitha <Lakshithagamer@gmail.com>
2023-05-12 14:08:28 +0530

    bug fixed, optimized and organized

--- src/lib/YT.js
+++ src/lib/YT.js
@@ -1,36 +1,41 @@
-const ytdl = require('youtubedl-core')
-const { createWriteStream, readFile } = require('fs-extra')
-const { tmpdir } = require('os')
-const axios = require('axios')
+const ytdl = require('youtubedl-core');
+const { validateURL, getInfo } = require('youtubedl-core');
+const { createWriteStream, readFile } = require('fs-extra');
+const { tmpdir } = require('os');
+const axios = require('axios');
+const crypto = require('crypto');
 
-const validateURL = (url) => ytdl.validateURL(url)
+// Generating a random file name
+const generateRandomFilename = (length) =>
+  crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length);
 
-const getInfo = async (url) => await ytdl.getInfo(url)
+//Downloading the video or audio file and returning it as a buffer
+const getBuffer = (url, type) => {
+  const filename = `${tmpdir()}/${generateRandomFilename(12)}.${type === 'audio' ? 'mp3' : 'mp4'}`; // generate a random file name
+  const stream = createWriteStream(filename);
+  ytdl(
+    url,
+    { filter: type === 'audio' ? 'audioonly' : 'videoandaudio' },
+    { quality: type === 'audio' ? 'highestaudio' : 'highest' }
+  ).pipe(stream); // Download the video or audio and pipe it to the write stream
+  return new Promise((resolve, reject) => {
+    stream.on('finish', () => {
+      readFile(filename).then(resolve).catch(reject);
+    });
+    stream.on('error', reject); // if there is an error with the write stream, reject the promise with the error
+  });
+};
 
-const getBuffer = async (url, type) => {
-    filename = `${tmpdir()}/${Math.random().toString(36)}.${type === 'audio' ? 'mp3' : 'mp4'}`
-    const stream = createWriteStream(filename)
-    ytdl(
-        url,
-        { filter: type === 'audio' ? 'audioonly' : 'videoandaudio' },
-        { quality: type === 'audio' ? 'highestaudio' : 'highest' }
-    ).pipe(stream)
-    filename = await new Promise((resolve, reject) => {
-        stream.on('finish', () => resolve(filename))
-        stream.on('error', (err) => reject(err && console.log(err)))
-    })
-    return await readFile(filename)
-}
-
+// parsing the video ID from a YouTube video URL
 const parseId = (url) => {
-    const split = url.split('/')
-    if (url.includes('youtu.be')) return split[split.length - 1]
-    return url.split('=')[1]
-}
+  const split = url.split('/'); 
+  if (url.includes('youtu.be')) return split[split.length - 1]; 
+  return url.split('=')[1]; 
+};
 
 module.exports = {
-    validateURL,
-    getInfo,
-    getBuffer,
-    parseId
-}
+  validateURL,
+  getInfo,
+  getBuffer,
+  parseId,
+};
