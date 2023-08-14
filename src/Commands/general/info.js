3e4a474781a6e3196b05bd5bb2134a93e0f4f396
Alι Aryαɴ <85696485+AliAryanTech@users.noreply.github.com>
2023-08-14 21:51:33 +0500

    nothing

--- src/Commands/general/info.js
+++ src/Commands/general/info.js
@@ -5,14 +5,7 @@ module.exports = {
     exp: 0,
     description: 'Get information bot information',
     async execute(client, flag, arg, M) {
-        const pad = (s) => (s < 10 ? '0' : '') + s
-        const formatTime = (seconds) => {
-            const hours = Math.floor(seconds / (60 * 60))
-            const minutes = Math.floor((seconds % (60 * 60)) / 60)
-            const secs = Math.floor(seconds % 60)
-            return `${pad(hours)}:${pad(minutes)}:${pad(secs)}`
-        }
-        const uptime = formatTime(process.uptime())
+        const uptime = client.utils.formatSeconds(process.uptime())
         const groups = await client.getAllGroups()
         const users = await client.getAllUsers()
         return void (await M.reply(
